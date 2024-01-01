28b328210d8f127bb1a49e984ba0a37712996378
maddi02 <martin.hummel02@icloud.com>
2024-01-01 18:52:12 +0100

    Add admin page access restriction
    Updated routing in the App.tsx to restrict access to the admin page. Now, only admins, identified by their email, can have access to the admin page. Non-admins are redirected to the home page.

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,28 @@ import ShoopingCard from "./components/ShoppingCard/ShoopingCard.tsx";
 import Register from "./components/Auth/Register.tsx";
 import UserProfile from "./components/Auth/UserProfile.tsx";
 import AdminPage from "./components/Products/AdminPage.tsx";
+import {useSelector} from "react-redux";
+import {RootState} from "./utilits/State/store.ts";
 
 export default function App() {
+  const user = useSelector((state: RootState) => state.auth.user);
+  const adminEmail = process.env.REACT_APP_ADMIN_EMAIL;
+
+  console.log(user?.email);
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<UserProfile/>} />
-        <Route path="/adminPage" element={<AdminPage/>} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route
+          path="/adminPage"
+          element={user?.email === adminEmail && user != undefined ? <AdminPage /> : <Navigate replace to="/home" />}
+        />
         <Route path="/home" element={<LayoutWithSidebar><Home /></LayoutWithSidebar>} />
-        <Route path="/orders" element={<LayoutWithSidebar><Orders/></LayoutWithSidebar>} />
-        <Route path="/shoppingCard" element={<LayoutWithSidebar><ShoopingCard/></LayoutWithSidebar>} />
+        <Route path="/orders" element={<LayoutWithSidebar><Orders /></LayoutWithSidebar>} />
+        <Route path="/shoppingCard" element={<LayoutWithSidebar><ShoopingCard /></LayoutWithSidebar>} />
         <Route index element={<Navigate replace to="/home" />} />
       </Routes>
     </Router>
